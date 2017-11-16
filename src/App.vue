1bb1cf3a8ac2ce3fded56cb45a4cd82b2481e01f
zhouchuang <zhouchuang1217@163.com>
2017-11-16 16:26:35 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <Toolbar></Toolbar>
+    <NoteList></NoteList>
+    <Editor></Editor>
+  </div>
+</template>
+
+<script>
+import Toolbar from './components/Toolbar.vue'
+import NoteList from './components/NoteList.vue'
+import Editor from './components/Editor.vue'
+export default {
+  name: 'app',
+  components: {
+    Toolbar,
+    NoteList,
+    Editor
+  }
+}
+</script>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Raleway:400,300);
+html, #app {
+  height: 100%;
+}
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  height: 100%;
+  max-height: 100%;
+  position: relative;
+}
+#toolbar {
+  float: left;
+  width: 80px;
+  height: 100%;
+  background-color: #30414D;
+  color: #767676;
+  padding: 35px 25px 25px 25px;
+}
+#notes-list {
+  float: left;
+  width: 300px;
+  height: 100%;
+  background-color: #F5F5F5;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 400;
+}
+#note-editor {
+  min-width: 600px;
+  height: 100%;
+  margin-left: 380px;
+}
+
+</style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,44 @@
+<template>
+ <div id="toolbar">
+    <i @click="addNote" class="glyphicon glyphicon-plus"></i>
+    <i @click="tooglefavorite" class="glyphicon glyphicon-star"></i>
+    <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+ </div> 
+</template>
+
+<script>
+export default {
+  name:'hello',
+  data(){
+      return {
+          msg:'Welcome to My vue.js App'
+      }
+  },
+  methods:{
+      addNote:function(){
+          console.log('addNote');
+      },
+      tooglefavorite:function(){
+          console.log('tooglefavorite');
+      },
+      deleteNote:function(){
+          console.log('deleteNote');
+      }
+  }
+}
+</script>
+
+<style>
+  #toolbar i {
+  font-size: 30px;
+  margin-bottom: 35px;
+  cursor: pointer;
+  opacity: 0.8;
+  transition: opacity 0.5s ease;
+}
+
+#toolbar i:hover {
+  opacity: 1;
+}
+</style>
+
